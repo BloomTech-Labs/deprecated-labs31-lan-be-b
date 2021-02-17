132a3b5c6d115a23d730bb02d6330f461a0f9b60
mmitch2958 <mikescottmitchell@outlook.com>
2021-02-17 12:58:33 -0500

    changed pull_request_temp.md name to fit standards

---
+++ seeds/01-users.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {id: 1, email: 'email@email.com', display_name:'Admin', profile_picture: 'picture', track: 'web', likes:'', onboarded: '', created_at, updated_at},
+        {id: 2, email: 'email@email.com', display_name:'Mod', profile_picture: 'picture', track: 'web', likes:'', onboarded: '', created_at, updated_at},
+        {id: 3, email: 'email@email.com', display_name:'User', profile_picture: 'picture', track: 'web', likes:'', onboarded: '', created_at, updated_at}
+      ]);
+    });
+};
