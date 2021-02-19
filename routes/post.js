395be2a9e1243201dbffc0b12ab6d4c69f63c1e8
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-19 13:57:53 -0500

    Removed category from Post.create call in routes/post.js

--- routes/post.js
+++ routes/post.js
@@ -7,9 +7,9 @@ const app = express.Router();
 // Create post
 app.post('/create', (request, response) => {
     const userID = request.user.id;
-    const { question, answer, track, category } = request.body;
+    const { question, answer, track } = request.body;
     
-    Post.create({ user_id: userID, question, answer, track, category })
+    Post.create({ user_id: userID, question, answer, track })
         .then(res => response.status(200).json({ message: 'Post created successfully' }))
         .catch(err => {
             console.log(err);
