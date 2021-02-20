2b68a43eb6de092d96e22af925a19536fb32e239
Natividad Alvarez <tivialvarez@gmail.com>
2021-02-19 22:07:26 -0500

    Adds seed data for rooms and adds room_id to posts

--- seeds/02-posts.js
+++ seeds/02-posts.js
@@ -4,9 +4,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('posts').insert([
-        {id: 1, user_id: 1, question:'jar gonj ar gon ja', answer:'jar gonj ar gon jajar gonj ar gon jajar gonj ar gon ja', likes:0, comments:0, track:0},
-        {id: 2, user_id: 2, question:'jar gonj ar gon ja', answer:'jar gonj ar gon jajar gonj ar gon jajar gonj ar gon ja', likes:0, comments:0, track:0},
-        {id: 3, user_id: 3, question:'jar gonj ar gon ja', answer:'jar gonj ar gon jajar gonj ar gon jajar gonj ar gon ja', likes:0, comments:0, track:0}
+        {id: 1, user_id: 1, room_id: 1, question:'jar gonj ar gon ja', answer:'jar gonj ar gon jajar gonj ar gon jajar gonj ar gon ja', likes:0, comments:0, track:0},
+        {id: 2, user_id: 2, room_id: 1, question:'jar gonj ar gon ja', answer:'jar gonj ar gon jajar gonj ar gon jajar gonj ar gon ja', likes:0, comments:0, track:0},
+        {id: 3, user_id: 3, room_id: 1, question:'jar gonj ar gon ja', answer:'jar gonj ar gon jajar gonj ar gon jajar gonj ar gon ja', likes:0, comments:0, track:0}
       ]);
     });
 };
